e61c559013619f173a5ebeaa870bf912cbbabf71
Phong Truong <phong@passioncorners.com>
2021-12-24 13:13:30 +0700

    curl options

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -49,10 +49,13 @@ return [
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 'encrypted' => true,
-                // 'host' => '127.0.0.1',
                 'host' => env('APP_URL'),
                 'port' => 6001,
-                'scheme' => 'http'
+                'scheme' => 'http',
+                'curl_options' => [
+                    CURLOPT_SSL_VERIFYHOST => 0,
+                    CURLOPT_SSL_VERIFYPEER => 0,
+                ]
             ],
         ],
 
